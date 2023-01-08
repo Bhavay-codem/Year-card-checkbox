c7c7cff4800434eace6734764a9355934383d2a8
Bhavay Sachdeva <90045529+Bhavay-codem@users.noreply.github.com>
2023-01-09 05:05:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// year2021
+$(document).ready(function () {
+    $(".number-one-one").on("input", function () {
+      $(".number-one-one").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-one-two").on("input", function () {
+      $(".number-one-two").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-one-three").on("input", function () {
+      $(".number-one-three").not($(this)).val($(this).val());
+    })
+})
+
+// year2022
+$(document).ready(function () {
+    $(".number-two-one").on("input", function () {
+      $(".number-two-one").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-two-two").on("input", function () {
+      $(".number-two-two").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-two-three").on("input", function () {
+      $(".number-two-three").not($(this)).val($(this).val());
+    })
+})
+
+// year2023
+$(document).ready(function () {
+    $(".number-three-one").on("input", function () {
+      $(".number-three-one").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-three-two").on("input", function () {
+      $(".number-three-two").not($(this)).val($(this).val());
+    })
+})
+
+$(document).ready(function () {
+    $(".number-three-three").on("input", function () {
+      $(".number-three-three").not($(this)).val($(this).val());
+    })
+})
+
+
+
+
